05ec85b765e7e47a813f867d61b860ceca46d7ec
Techinventor <krisdavy1@gmail.com>
2021-08-29 12:40:12 +0100

    fixed sidebar

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function openNav(){
+    document.getElementById("sidenav_act").style.width= "300px";
+    
+}
+
+function closeNav(){
+    document.getElementById("sidenav_act").style.width= "0";
+}
